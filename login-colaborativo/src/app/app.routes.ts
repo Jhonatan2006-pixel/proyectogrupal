88e1a58684e4f72a4a4108b6d2f30a476f84adb3
lehcer <rechellemus130@gmail.com>
2026-02-09 03:35:05 -0600

    paso 4

--- login-colaborativo/src/app/app.routes.ts
+++ login-colaborativo/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+ 
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
