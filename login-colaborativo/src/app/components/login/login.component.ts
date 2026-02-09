9637ca159e8e0c8f098f9cfbf5be765de630bbc7
skill-informatic <jozsefrn@gmail.com>
2026-02-09 16:21:39 -0600

    paso 5

--- login-colaborativo/src/app/components/login/login.component.ts
+++ login-colaborativo/src/app/components/login/login.component.ts
@@ -1,69 +1,49 @@
 import { Component } from '@angular/core';
-
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-
-import { MatInputModule } from '@angular/material/input';
-
-import { MatFormFieldModule } from '@angular/material/form-field';
-
-import { MatButtonModule } from '@angular/material/button';
-
 import { CommonModule } from '@angular/common';
  
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+ 
+import { Login } from '../../interfaces/loginDto';
+ 
 @Component({
-
   selector: 'app-login',
-
   standalone: true,
-
   imports: [
-
     CommonModule,
-
     ReactiveFormsModule,
-
     MatFormFieldModule,
-
     MatInputModule,
-
-    MatButtonModule
-
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ],
-
   templateUrl: './login.component.html',
-
-  styleUrls: ['./login.component.css']
-
+  styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent {
  
   loginForm: FormGroup;
  
   constructor(private fb: FormBuilder) {
-
     this.loginForm = this.fb.group({
-
       username: ['', Validators.required],
-
-      password: ['', Validators.required]
-
+      password: ['', Validators.required],
     });
-
   }
  
-  login(){
-
-    if(this.loginForm.invalid) return;
+  login() {
+    if (this.loginForm.invalid) return;
  
-    const { username, password } = this.loginForm.value;
-
-    console.log('Usuario:', username);
-
-    console.log('Password:', password);
-
+    const objectRequest: Login = {
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password
+    };
+ 
+    console.log('Login object:', objectRequest);
   }
-
 }
-
- 
