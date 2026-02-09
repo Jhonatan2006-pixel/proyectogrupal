a5fddc8455c0fe60dea39b66701d598bcc53f613
lehcer <rechellemus130@gmail.com>
2026-02-09 01:17:13 -0600

    Paso 1

--- login-colaborativo/src/app/components/login/login.component.ts
+++ login-colaborativo/src/app/components/login/login.component.ts
@@ -1,35 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Login } from '../../interfaces/loginDto';
-import { LoginService } from '../../services/login/login.service';
-
+import { Login } from 'src/app/interfaces/loginDto';
+ 
+ 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  
-  private loginService = inject(LoginService);
-
+ 
   username: string = '';
   password: string = '';
-
+ 
   login() {
     console.log('Usuario:', this.username);
     console.log('Password:', this.password);
-
-    if (this.username !== '' && this.password !== '') {
-      const objectRequest: Login = {
-        username: this.username,
-        password: this.password 
-      };
-
-      this.loginService.doLogin(objectRequest).subscribe(entry => {
-        if (entry) {
-          console.log("login exitoso");
-        }
-      });
-    }
-
+    const objectRequest: Login = {
+      username: this.username,
+      password: this.password
+    };
+ 
   }
 }
